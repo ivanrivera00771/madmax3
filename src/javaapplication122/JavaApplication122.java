3e1dc66b755aa901e8d5d14b7d6f938758535d96
IvanRivera771 <47260927+IvanRivera771@users.noreply.github.com>
2020-12-05 16:25:40 -0600

    PRIMER APORTE

---
+++ src/javaapplication122/JavaApplication122.java
@@ -0,0 +1,11 @@
+
+package javaapplication122;
+
+public class JavaApplication122 {
+    
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+     
+    }
+    
+}
